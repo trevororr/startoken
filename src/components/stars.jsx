a758d3da4259746bfb064313058bf8ff3ee68a8c
TheRealDeathEye <therealdeatheye@gmail.com>
2021-12-24 04:40:35 -0600

    starsGenerated

--- src/components/stars.jsx
+++ src/components/stars.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
+async function getStars(){
+  let response = await fetch('./starData.json')
+  return await response.json();
+}
+
 function Stars() {
+  var stars = getStars();
+  console.log(stars);
+  // const starData = stars.map((number) =>
+  //   <li key={number[0].toString()}>
+  //     {number}
+  //   </li>
+  // );
+
   return (
     <div align='center'>
         <h1 id="titleText">Stars</h1>
+        {/* <h2>{starData}</h2> */}
+        
     </div>
   );
 }

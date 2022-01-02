85af49b22f301974ed6fb8ca938588d0b34c549d
TheRealDeathEye <therealdeatheye@gmail.com>
2022-01-02 01:33:34 -0600

    broken-SearchBarImp

--- data/addImgfiles.py
+++ data/addImgfiles.py
@@ -6,7 +6,7 @@ print(items)
 mapping = {}
 for filename in enumerate(items):
     name = filename[1].split('.')[0]
-    mapping[name] = ('https://gateway.pinata.cloud/ipfs/Qme4JEXh5seBfTJ66fbLAV48S1A3mKhbwdfgGVGhMbF5WZ/'+name+'.png')
+    mapping[name] = (name+'.png')
 print(mapping)
 with open('imgData.json', 'w') as f:
     f.write(json.dumps(mapping))
--- src/components/stars.jsx
+++ src/components/stars.jsx
@@ -5,8 +5,17 @@ import {Form,Card} from 'react-bootstrap';
 
 
 function Stars() {
+  function importAll(r) {
+    let images = {};
+     r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images
+   }
+  const images = importAll(require.context('../stars', false, /\.(png|jpe?g|svg)$/));
+  console.log(images);
+
   const [allStars, setAllStars]=useState([]);
   const [starCount,setStarCount]=useState(20);
+  const [search, setSearch] = useState('');
 
   const loadMore = () => {
     setStarCount(starCount+10)
@@ -16,31 +25,31 @@ function Stars() {
     fetch("/data/starData.json")
       .then((res)=>res.json())
       .then((data)=>{
-        console.log(data);
         setAllStars(data)
       });
   },[])
 
   const stars = Object.keys(allStars).slice(0,starCount).map((key)=>{
-    console.log("image updated")
     return(
       <div key={key} className='col'>
         <Card style={{width:'100px', background:'#00000000', paddingBottom:'25px'}}>
-          <img width="100" height="100" key={key} alt={key} src={allStars[key]}/>
+          <img width="100" height="100" key={key} alt={key} src={images[allStars[key]]}/>
         </Card>
       </div>
-    )});
+  )});
+
+  const filteredStars = search.length === 0 ? stars : stars.filter(search.toLowerCase())
   
   return (
     <>
     <div className='centered'>
       
-      <Form className='foreground'><Form.Control placeholder="Star Name" style={{backgroundColor:'#000000',color:'#ffffff'}} id='nameBar' /></Form>
+      <Form className='foreground'><Form.Control onChange={(e) => setSearch(e.target.value)} placeholder="Star Name" style={{backgroundColor:'#000000',color:'#ffffff'}} id='nameBar' /></Form>
     </div>
     <div className='starContainer'>
     <div className='row' style={{width:'80%'}}>
-      {stars}
-      <button onClick={loadMore} style={{zIndex:'950'}}>loadmore</button>
+      {filteredStars}
+      <button onClick={loadMore} style={{zIndex:'950'}} id='loadButton'>loadmore</button>
     </div>
     </div>
     </>

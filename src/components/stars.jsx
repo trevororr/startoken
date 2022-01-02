ca72285688d06ee1a0b4727a09cdd752557e2964
TheRealDeathEye <therealdeatheye@gmail.com>
2022-01-01 23:37:21 -0600

    starsLoadMore

--- src/components/stars.jsx
+++ src/components/stars.jsx
@@ -1,48 +1,36 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Form,Card} from 'react-bootstrap';
 
 
 
 
 function Stars() {
-  const useGetStars = async function() {
-    console.log("get stars");
-    const [data, setData]=useState({})
-    console.log(data);
-    if(Object.keys(data).length === 0){
-      console.log("data is empty");
-      console.log(data);
-      return [];
-    }
-    console.log("little further")
-    const response = await fetch("/data/starData.json")
+  const [allStars, setAllStars]=useState([]);
+  const [starCount,setStarCount]=useState(20);
 
-    const tempData =  JSON.parse(await response.text())
-    console.log("HERE");
-    const imgs = Object.keys(tempData).map((key)=>{
-            return(
-              <div>
-                <Card style={{width:'100px', background:'#00000000', paddingBottom:'25px'}}>
-                  <img width="100" height="100" key={key} alt={key}src={tempData[key]}/>
-                </Card>
-              </div>
-            )});
-    setData(imgs);
-    return imgs;
-  }  
+  const loadMore = () => {
+    setStarCount(starCount+10)
+  };
+
+  useEffect(()=>{
+    fetch("/data/starData.json")
+      .then((res)=>res.json())
+      .then((data)=>{
+        console.log(data);
+        setAllStars(data)
+      });
+  },[])
+
+  const stars = Object.keys(allStars).slice(0,starCount).map((key)=>{
+    console.log("image updated")
+    return(
+      <div key={key} className='col'>
+        <Card style={{width:'100px', background:'#00000000', paddingBottom:'25px'}}>
+          <img width="100" height="100" key={key} alt={key} src={allStars[key]}/>
+        </Card>
+      </div>
+    )});
   
-  
-
-  const data = useGetStars();
-  const [display, setDisplay] = useState([]);
-
-  function loadMore(){
-    console.log('here');
-    console.log(data);
-    setDisplay(data.slice(0,display.length+5));
-  }
-  
-
   return (
     <>
     <div className='centered'>
@@ -51,8 +39,8 @@ function Stars() {
     </div>
     <div className='starContainer'>
     <div className='row' style={{width:'80%'}}>
-      {display}
-      <button onClick={loadMore}>loadmore</button>
+      {stars}
+      <button onClick={loadMore} style={{zIndex:'950'}}>loadmore</button>
     </div>
     </div>
     </>
